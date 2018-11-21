31daeb676202f221eb1b92cde0fa90c97c71d487
strix <damjan.oslaj@gmail.com>
2018-11-21 17:46:32 +0100

    .

---
+++ src/containers/App.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
+
+class App extends Component {
+
+    componentDidMount(){
+      this.props.onRequestRobots();
+    }
+
+
+    render(){
+        const { searchField, onSearchChange, robots, isPending } = this.props;
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+
+        if(isPending){
+            return <h1>Loading</h1>
+        }else{
+            return ( 
+                <div className="tc">
+                <h1 className="f1">Robots</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                  <ErrorBoundry>
+                    <CardList robots={filterRobots} />
+                  </ErrorBoundry>
+                </Scroll>
+                </div>
+            );
+        }
+    };
+}
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import App from './containers/App';
+import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider> 
+  , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
